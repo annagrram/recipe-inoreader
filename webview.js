dbf75b5d3a688945962add5cbbc32a334497663e
Anna Arad <anna.a@qspark.co>
2021-05-26 22:42:21 +0200

    Fix unread number in badge for many unread articles

--- webview.js
+++ webview.js
@@ -3,7 +3,7 @@ module.exports = (Franz) => {
         const all_articles = document.querySelector('#unread_cnt_all_items')
         if (!all_articles) return;
 
-        const unread_articles_cnt = Number(all_articles.textContent)
+        const unread_articles_cnt = Number(all_articles.textContent.split('+')[0])
         if (!Number.isNaN(unread_articles_cnt)) {
             Franz.setBadge(unread_articles_cnt);
         }
